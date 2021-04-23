f805d8fe9d974d8e421e2c7e57a331c9569b6336
盧露 <low.lucyy@gmail.com>
2021-04-22 23:02:56 -0400

    Update voice_recognition.js

--- NLP_tensorflow_models/voice_recognition.js
+++ NLP_tensorflow_models/voice_recognition.js
@@ -1,4 +1,4 @@
-<button type="button" onclick="init()">Start</button>
+<button type="button" onclick="init()">Start NLP</button>
 <div id="label-container"></div>
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/speech-commands@0.4.0/dist/speech-commands.min.js"></script>
